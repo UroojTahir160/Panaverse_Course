7550691dd201720316f7ed469b1f14790af69127
UroojTahir160 <uroojt113@gmail.com>
2023-09-01 22:20:51 +0500

    commented lines in quiz

--- quiz1/quiz1_prep.ts
+++ quiz1/quiz1_prep.ts
@@ -27,3 +27,60 @@ interface NumberDictionary {
   // name: string;
   //   Property 'name' of type 'string' is not assignable to 'string' index type 'number'.
 }
+
+// Any
+
+let myval: any;
+let myVal2: unknown;
+let neverVal: never;
+
+myval = true; // OK
+myval = 42; // OK
+myval = "hey!"; // OK
+myval = []; // OK
+myval = {}; // OK
+myval = Math.random; // OK
+myval = null; // OK
+myval = undefined; // OK
+myval = () => {
+  console.log("Hey again!");
+}; // OK
+let myUnknownValue: unknown = myval; // throw error
+myval = myVal2;
+// let boolVal: boolean = myVal2;
+// let numVal: string = neverVal;
+// let boolVal2: boolean = true;
+// neverVal = myval;
+
+// myVal2 = boolVal;
+// myVal2 = numVal;
+
+myval = myVal2;
+
+myVal2 = myval;
+
+let input = document.querySelector('input["type="text"]') as HTMLInputElement;
+console.log(input.value);
+
+//Unknown
+
+let value: unknown;
+
+value = true; // OK
+value = 42; // OK
+value = "hey!"; // OK
+value = []; // OK
+value = {}; // OK
+value = Math.random; // OK
+value = null; // OK
+value = undefined; // OK
+value = () => {
+  console.log("Hey again!");
+}; // OK
+
+//initializing the length variable which is of number type
+let stringLength: number = 22;
+
+//initializing the flower variable which is of string type
+let flower: string = stringLength as unknown as string;
+console.log("The length of the string is: ", stringLength);
